23dcd39685be1891898efc586a23ff37324b4682
Alro John <alromercado08@gmail.com>
2025-03-17 22:35:35 +0800

    push all to fix bugs

--- src/components/custom/custom-ui/navbar-home.tsx
+++ src/components/custom/custom-ui/navbar-home.tsx
@@ -72,7 +72,7 @@ const Navbar = () => {
             {!isLoggedIn ? (
               <Link
                 href="/auth"
-                className="ml-6 w-24 inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                className=" w-24 inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
               >
                 Login
               </Link>
