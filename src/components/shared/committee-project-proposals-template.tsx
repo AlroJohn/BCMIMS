9ba46a17b928055a639d99e1bc57881f5570511b
Alro John <alromercado08@gmail.com>
2025-03-20 16:57:45 +0800

    no action showed

--- src/components/shared/committee-project-proposals-template.tsx
+++ src/components/shared/committee-project-proposals-template.tsx
@@ -472,7 +472,7 @@ export default function CommitteeProjectProposalsTemplate({
             {project.rejectionReason || "No reason provided"}
           </span>
         );
-      case "actions":
+      case "remarks":
         return (
           <ActionButtons
             project={project}
